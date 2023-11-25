0d4a860acdaf393d551840af0be9abb20eb25304
Nolan Gelinas <nolangelinas@gmail.com>
2023-11-25 01:24:10 -0500

    Update example and add error checking

--- src/auth/user.ts
+++ src/auth/user.ts
@@ -2,15 +2,14 @@ import { decode } from "next-auth/jwt";
 import "dotenv/config";
 
 export async function getUser(sessionToken: string) {
-	// console.log("sessionToken", sessionToken);
+	if (!process.env.NEXTAUTH_SECRET)
+		throw new Error("Set NEXTAUTH_SECRET env variable");
 	try {
 		// Verify the token
 		const decodedToken = await decode({
 			token: sessionToken,
 			secret: process.env.NEXTAUTH_SECRET,
 		});
-
-		// console.log("decodedToken", decodedToken);
 
 		if (!decodedToken) return null;
 		// Retrieve the user's information
